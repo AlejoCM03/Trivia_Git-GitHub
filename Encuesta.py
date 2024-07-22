bd5131941d43a786b9296b59875d98627dab7155
Alan Vilchis <alanvilchis07@gmail.com>
2024-07-21 18:12:41 -0600

    commit provisional hasta septima pregunta e imágenes

--- Encuesta.py
+++ Encuesta.py
@@ -1,10 +1,15 @@
 import sys
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 print("******************Bienvenido al sistema de trivia de Git 1****************** ")
 print("En esta trivia veremos que tanto conoces acerca de videojuegos")
 i=1
 print("-------------------------------------------------------------------------------")
 print("Primera pregunta:")
 print("¿Quién es el personaje principal de Halo?\n 1.-Marcus Fénix\n 2.-Solid Snake\n 3.- Mater Chief \n 4.- The Doomslayer \n 5.- Siguiente pregunta \n 6.- Salir. \n")
+img=mpimg.imread("1.jpg")
+imgplot=plt.imshow(img)
+plt.show()
 while(1):
     opcion=0 
     print("\t\tIntento número",i)
@@ -103,6 +108,26 @@ while(1):
         print("Introduce una opción de las que se muestran en pantalla.")
 print("-------------------------------------------------------------------------------")
 print("Sexta pregunta:")
+print("¿Quién es el desarrollador principal de la serie de videojuegos Dark Souls?")
+print("1.- Capcom \n 2.- Bethesda Game Studios \n 3.- FromSoftware \n 4.- CD Projekt Red \n 5.- Siguiente pregunta \n 6.- Salir. \n")
+while(1):
+    opcion=0 
+    print("\t\tIntento número",i)
+    opcion= int(input("Seleccione la opción:"))
+    i+=1
+    if opcion==1 or opcion==2 or opcion==4:
+        print("Error, intenta de nuevo")
+    elif opcion==3:
+        print("¡Correcto!")
+        break
+    elif opcion==5:
+        break
+    elif opcion==6:
+        exit()
+    else:
+        print("Introduce una opción de las que se muestran en pantalla.")
+print("-------------------------------------------------------------------------------")
+print("Septima pregunta:")
 print("¿Quién es el desarrollador principal de la serie de videojuegos Dark Souls?")
 print("1.- Capcom \n 2.- Bethesda Game Studios \n 3.- FromSoftware \n 4.- CD Projekt Red \n 5.- Siguiente pregunta \n 6.- Salir. \n")
 while(1):
